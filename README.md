eafd5d2854fde8b509f32e2c992cd8c75246d5eb
Chris Heppell <clheppell@icloud.com>
2018-08-25 16:51:32 +0100

    Getting started

---
+++ README.md
@@ -0,0 +1,4 @@
+# EquivalentTime
+An in progress class based implementation of the British Swimming/SPORTSYSTEMS Equivalent Time Algorithm
+
+This really is in progress
---
+++ src/CLSASC/EquivalentTime.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace CLSASC\EquivalentTime;
+
+/**
+ * A terribly useful class
+ *
+ * See: http://knpuniversity.com/screencast/question-answer-day
+ */
+class EquivalentTime
+{
+    public function main()
+    {
+        return null;
+    }
+}
