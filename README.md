57700078483272558d854881df285a54f4001d1b
Chris Heppell <clheppell@icloud.com>
2018-08-25 22:22:09 +0100

    Pretty much ready

--- README.md
+++ README.md
@@ -1,4 +1,23 @@
 # EquivalentTime
-An in progress class based implementation of the British Swimming/SPORTSYSTEMS Equivalent Time Algorithm
+A class based implementation of the British Swimming/SPORTSYSTEMS Equivalent
+Time Algorithm. Written in-house by Chester-le-Street ASC.
 
-This really is in progress
+This library has not yet undergone full testing, so use with caution for now.
+
+## Basic Usage
+1. Install via composer
+```
+composer require clsasc/equivalent-time
+```
+2. From there, create an EquivalentTime Object in your project
+```PHP
+$object = new EquivalentTime("25m", "200 Free", 116.68);
+```
+Parameter 1 is the length of the source pool, Parameter 2 is the event and Parameter 3 is the time, formatted as a float, `seconds.hundreds`.
+3. Choose if you want the output to be formatted as a string or a float. Float output is default, but you can get it as an 'alpha format' string by invoking the `setOutputAsString(true);` method on your object
+4. Get your output.
+```PHP
+$obj->getConversion("50m");
+```
+Where the parameter is the length of pool you would like your time to be converted to.
+5. You can call the `getConversion($length)` method on your conversion object as many times as you wish. To convert another time, create a new object and the old one will be garbage collected by PHP after it is no longer referenced.
--- src/CLSASC/EquivalentTime/EquivalentTime.php
+++ src/CLSASC/EquivalentTime/EquivalentTime.php
@@ -15,15 +15,16 @@ require 'PoolMeasure.php';
  */
 class EquivalentTime {
   private $source_pool_length;
-  private $target_pool_length;
   private $time_source;
   private $time_50;
-  private $time_target;
   private $event;
+  private $distance;
   private $pool_length_flag;
   private $turns_per_hundred;
+  private $formattedString;
 
   public function __construct($source_pool_length, $event, $time_source) {
+    $this->formattedString = false;
     // Check pool length
     $this->pool_length_flag = PoolLengthParameters::getFlag($source_pool_length);
     $this->turns_per_hundred = Turns::perHundred($source_pool_length);
@@ -48,20 +49,22 @@ class EquivalentTime {
 
     if ($this->turns_per_hundred == 1) {
       $this->time_50 = $time_source;
+      $this->distance = EVENT::distance($this->event);
     } else {
       // Get PoolMeasure
       $pool_measure = PoolMeasure::getValue($this->source_pool_length, $this->event);
 
-      $distance = EVENT::distance($this->event);
+      $this->distance = EVENT::distance($this->event);
+
       $d1;
       // Numb turn factor
-      if ($distance == 1500 && $this->pool_length_flag == 3) {
+      if ($this->distance == 1500 && $this->pool_length_flag == 3) {
         $d1 = 1650; // Yards Equivalent !!! UNKNOWN MEANING
       } else {
-        $d1 = $distance;
+        $d1 = $this->distance;
       }
 
-      $num_turn_factor = $distance/100*($d1/100)*($this->turns_per_hundred-1);
+      $num_turn_factor = $this->distance/100*($d1/100)*($this->turns_per_hundred-1);
       $turn_factor = Turns::getFactor($this->event);
 
       $this->time_50 = ($this->time_source + sqrt($this->time_source *
@@ -69,11 +72,47 @@ class EquivalentTime {
       (2 * $pool_measure);
     }
   }
+
+  public function getConversion($target_pool_length) {
+    $pool_length_flag = PoolMeasure::getValue($target_pool_length, $this->event);
+    $pool_measure = PoolMeasure::getValue($target_pool_length, $this->event);
+    $turns_per_hundred = Turns::perHundred($target_pool_length);
+    $turn_factor = Turns::getFactor($this->event);
+    $turn_value = $turn_factor / $this->time_50;
+
+    // Test for special case 1500 in yards pool
+    $imperial_distance = $this->distance;
+    if ($this->distance == 1500 && $this->pool_length_flag == 3) {
+      $imperial_distance = 1650;
+    }
+
+    $dist_time = $this->time_50 * $pool_measure;
+    $turn_time = $turn_value * ($imperial_distance / 100) * ($turns_per_hundred - 1);
+    $conversion_time = $dist_time - $turn_time + 0.05;
+
+    $floored_time = floor(($conversion_time*10))/10;
+
+    if ($this->formattedString) {
+      $seconds = (int) $floored_time;
+      $mins = (int) ($seconds / 60);
+      $hunds = $floored_time - $seconds;
+      $seconds = $seconds - $mins*60;
+      return sprintf('%02d', $mins) . ":" . sprintf('%02d', $seconds) . ":" . round($hunds*10, 1);
+    } else {
+      return $floored_time;
+    }
+  }
+
+  public function setOutputAsString($boolean) {
+    $this->formattedString = $boolean;
+  }
 }
 
 try {
-  $obj = new EquivalentTime("25m", "100 Free", 55.12);
+  $obj = new EquivalentTime("25m", "200 Free", 116.68);
 } catch (Exception $obj) {
   var_dump($obj);
 }
 var_dump($obj);
+$obj->setOutputAsString(true);
+echo $obj->getConversion("50m");
