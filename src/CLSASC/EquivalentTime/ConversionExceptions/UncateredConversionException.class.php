0b1f3928faf99473a4ebf8895fcee10df6f2412e
Chris Heppell <clheppell1@sheffield.ac.uk>
2019-01-09 19:07:31 +0000

    New Error Exceptions

---
+++ src/CLSASC/EquivalentTime/ConversionExceptions/UncateredConversionException.class.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace CLSASC\EquivalentTime\ConversionExceptions;
+
+/**
+ * The UncateredConversionException class
+ *
+ * @copyright Chester-le-Street ASC https://github.com/Chester-le-Street-ASC
+ * @author Chris Heppell https://github.com/clheppell
+ */
+class UncateredConversionException extends \Exception {
+  // Redefine the exception so message isn't optional
+  public function __construct($message, $code = 0, Exception $previous = null) {
+    parent::__construct($message, $code, $previous);
+  }
+
+  // custom string representation of object
+  public function __toString() {
+    return __CLASS__ . ": [{$this->code}]: {$this->message}\n";
+  }
+}
